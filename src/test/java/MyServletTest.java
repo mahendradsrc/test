bda1713d97a0a0d51bf378c643400f50a3bd4cff
mahendradsrc <40471424+mahendradsrc@users.noreply.github.com>
2018-07-24 16:20:49 +0530

    Add files via upload

---
+++ src/test/java/MyServletTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyServletTest  {
+
+	@Test
+	public void test() {
+		 
+		System.out.println("test executed");
+		assertTrue(true);
+	}
+
+}
